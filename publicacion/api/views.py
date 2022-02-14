babde6fed13105d399c7a7522f4938e93582456b
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-14 16:17:04 -0300

    setting permissions

--- publicacion/api/views.py
+++ publicacion/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from publicacion.api.serializers import PublicacionSerializer
 from publicacion.models import Publicacion
+from publicacion.api.permissions import IsAdminOrReadOnly
 
 
 class PublicacionView(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = PublicacionSerializer
     queryset = Publicacion.objects.all()
