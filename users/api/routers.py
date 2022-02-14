a670ffc0e52647cca7b246a7729eaaa02761cdc7
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-14 15:40:41 -0300

    create user

--- users/api/routers.py
+++ users/api/routers.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view()),
+    path('users/register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/me', UserView.as_view(), name='UserView'),
--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    pass
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
